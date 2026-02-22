df704c877cd484fdbdff322d4186e5b4af75c207
William SIMON--VEZO <william.simonvezo@gmail.com>
2026-02-22 14:37:27 +0100

    test

--- lib/image-loader.ts
+++
@@ -1,9 +0,0 @@
-const basePath =
-  process.env.NODE_ENV === "production" ? "/code-and-sorcery-website" : "";
-
-export default function imageLoader({ src }: { src: string }) {
-  if (src.startsWith("/")) {
-    return `${basePath}${src}`;
-  }
-  return src;
-}
--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  basePath:
-    process.env.NODE_ENV === "production" ? "/code-and-sorcery-website" : "",
   images: {
-    loader: "custom",
-    loaderFile: "./lib/image-loader.ts",
+    unoptimized: true,
   },
 };
 
