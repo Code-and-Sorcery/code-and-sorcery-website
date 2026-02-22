df540af753db5a726f4f5f08e82f4702aa8ad4ef
William SIMON--VEZO <william.simonvezo@gmail.com>
2026-02-22 12:45:36 +0100

    github workflow static export

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export",
+  basePath:
+    process.env.NODE_ENV === "production" ? "/code-and-sorcery-website" : "",
+  images: {
+    unoptimized: true,
+  },
+};
 
 export default nextConfig;
